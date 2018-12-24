7770904ce629d3f49b193b70c640c422d3dacf07
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2018-12-24 16:43:55 +0700

    .

--- resources/views/hrd/manajemen_scoreboard_kpi/manajemen_scoreboard_kpi.blade.php
+++ resources/views/hrd/manajemen_scoreboard_kpi/manajemen_scoreboard_kpi.blade.php
@@ -7,7 +7,7 @@
 <!-- partial -->
 <div class="content-wrapper">
 	<div class="row">
-		<div class="col-lg-12">	
+		<div class="col-lg-12">
 			<nav aria-label="breadcrumb" role="navigation">
 				<ol class="breadcrumb bg-info">
 					<li class="breadcrumb-item"><i class="fa fa-home"></i>&nbsp;<a href="#">Home</a></li>
