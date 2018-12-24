b18c62bdc931a0b880acdedca94254318d7b555b
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2018-12-24 17:46:03 +0700

    .

--- resources/views/hrd/manajemen_scoreboard/manajemen_scoreboard.blade.php
+++ resources/views/hrd/manajemen_scoreboard/manajemen_scoreboard.blade.php
@@ -1,7 +1,6 @@
 @extends('main')
 @section('content')
 
-@include('hrd/manajemen_scoreboard/tambah_manajemen_scoreboard')
 <!-- partial -->
 <div class="content-wrapper">
 	<div class="row">
@@ -82,17 +81,18 @@
 	</div>
 </div>
 
-<div class="modal fade" id="modal_detail_data" role="dialog">
-  <div class="modal-dialog" style="width: 70%;margin: auto;">
+<div id="modal_detail_data" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header bg-gradient-primary">
+        <h4 class="modal-title">Tambah Data Scoreboard</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
     <form method="post" id="form-detail-kpi" name="formDetailKpi">
       {{ csrf_field() }}
       <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header" style="background-color: #e77c38;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title" style="color: white;">Detail Data KPI</h4>
-        </div>
 
         <div class="modal-body">
           <div class="col-md-12 col-sm-12 col-xs-12 tamma-bg" style="padding-bottom: 10px;margin-bottom: 15px;">
@@ -139,17 +139,18 @@
   </div>
 </div>
 
-<div class="modal fade" id="modal_edit_data" role="dialog">
-  <div class="modal-dialog" style="width: 70%;margin: auto;">
+<div id="modal_edit_data" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header bg-gradient-primary">
+        <h4 class="modal-title">Tambah Data Scoreboard</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
     <form method="post" id="form-edit-kpi" name="formEditKpi">
       {{ csrf_field() }}
       <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header" style="background-color: #e77c38;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title" style="color: white;">Edit Data KPI</h4>
-        </div>
 
         <div class="modal-body">
           <div class="col-md-12 col-sm-12 col-xs-12 tamma-bg" style="margin-top:10px;padding-bottom: 10px;padding-top: 20px;margin-bottom: 15px;">
@@ -218,17 +219,18 @@
   </div>
 </div>
 
-<div class="modal fade" id="modal_tambah_data" role="dialog">
-  <div class="modal-dialog" style="width: 70%;margin: auto;">
+<div id="modal_tambah_data" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header bg-gradient-primary">
+        <h4 class="modal-title">Tambah Data Scoreboard</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
     <form method="post" id="form-input-kpi" name="formInputKpi">
       {{ csrf_field() }}
       <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header" style="background-color: #e77c38;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title" style="color: white;">Input Data KPI</h4>
-        </div>
 
         <div class="modal-body">
           <div class="col-md-12 col-sm-12 col-xs-12 tamma-bg" style="margin-top:10px;padding-bottom: 10px;padding-top: 20px;margin-bottom: 15px;">
