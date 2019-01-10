b53bc61f2d4b46e4321e24d39054a022072fbfa1
David Ridwan <ddavidridwan@gmail.com>
2019-01-09 22:56:37 -0800

    styles dashboard done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -184,8 +184,50 @@
                                 <span class="mdi mdi-chart-areaspline" style="font-size:4em; color:white;"></span>
                             </div>
                             <div class="col-lg-9" style="color:white;">
-                              <h1 class="text-right">0 H-P</h1>
-                              <p class="text-right">0 H-P</p>
+                              <h1 class="text-right">0 Hutang</h1>
+                              <p class="text-right">0 Hutang</p>
+                            </div>
+                          </div>
+                          <hr style="background:white; border:0px; height:1px; margin-bottom: 5px;">
+                          <div class="col-lg-5" style="color:white;">
+                              <p style="padding:0px 0px 0px; margin-bottom:5px;"><b>0 Released</b></p>
+                              <p style="padding:0px 0px 0px; margin-bottom:-20px;">0 WON</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-5" style="margin-left:3em; margin-top:1em;">
+                  <div class="card px-2" style="background-color:#ffe98a;">
+                  <i class="fa fa-times d-flex justify-content-end" aria-hidden="true" style="margin-top:5px; color:white;"></i>
+                      <div class="card-body">
+                          <div class="container-fluid d-flex justify-content-between">
+                            <div class="col-lg-3 pl-0">
+                                <span class="mdi mdi-chart-areaspline" style="font-size:4em; color:white;"></span>
+                            </div>
+                            <div class="col-lg-9" style="color:white;">
+                              <h1 class="text-right">0 Piutang</h1>
+                              <p class="text-right">0 Piutang</p>
+                            </div>
+                          </div>
+                          <hr style="background:white; border:0px; height:1px; margin-bottom: 5px;">
+                          <div class="col-lg-5" style="color:white;">
+                              <p style="padding:0px 0px 0px; margin-bottom:5px;"><b>0 Released</b></p>
+                              <p style="padding:0px 0px 0px; margin-bottom:-20px;">0 WON</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-5" style="margin-left:3em; margin-top:1em;">
+                  <div class="card px-2" style="background-color:#d195f9;">
+                  <i class="fa fa-times d-flex justify-content-end" aria-hidden="true" style="margin-top:5px; color:white;"></i>
+                      <div class="card-body">
+                          <div class="container-fluid d-flex justify-content-between">
+                            <div class="col-lg-3 pl-0">
+                                <span class="mdi mdi-chart-areaspline" style="font-size:4em; color:white;"></span>
+                            </div>
+                            <div class="col-lg-9" style="color:white;">
+                              <h1 class="text-right">0 Omset</h1>
+                              <p class="text-right">0 Omset</p>
                             </div>
                           </div>
                           <hr style="background:white; border:0px; height:1px; margin-bottom: 5px;">
@@ -198,47 +240,32 @@
               </div>
           </div>
           <hr style="background:#5c848e; border:0px; height:2px; margin-bottom: 5px;">
-          <!-- <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
-                <div class="card px-2" style="background-color:#0033c7;">
-                    <div class="card-body">
-                        <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-0 click">
-                            <span class="fa fa-star-o" style="color:white;"></span>
-                              <div class="ring"></div>
-                              <div class="ring2"></div>
-                            </div>
-                            <div class="col-lg-12" style="color:white;">
-                              <h1 style="margin-top:15px;">QO</h1>
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div> -->
           <div class="row">
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-2" style="background-color:#6c5ce7;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-3px;"></i>
                       <div class="card-body bintang1">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-format-quote-close" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">QO</h3>
+                              <h3 style="margin-top:15px;"  onclick="window.location.href='{{route('q_quotation')}}'">QO</h3>
                             </div>
                           </div>
                       </div>
                   </div>
               </div>
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
-                  <div class="card px-2" style="background-color:#ff7675;">
+                  <div class="card px-3" style="background-color:#ff7675;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-10px;"></i>
                       <div class="card-body bintang2">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-cart" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">SO</h3>
+                              <h3 style="margin-top:15px;" onclick="window.location.href='{{route('s_order')}}'">SO</h3>
                             </div>
                           </div>
                       </div>
@@ -246,27 +273,29 @@
               </div>
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-2" style="background-color:#00b894;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-3px;"></i>
                       <div class="card-body bintang3">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-cart" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">WO</h3>
+                              <h3 style="margin-top:15px;" onclick="window.location.href='{{route('w_order')}}'">WO</h3>
                             </div>
                           </div>
                       </div>
                   </div>
               </div>
-              <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
-                  <div class="card px-2" style="background-color:#fdcb6e;">
-                      <div class="card-body bintang4">
+              <div class="col-lg-3 col-sm-4" style="margin-left:3em; margin-top:1em;">
+                  <div class="card px-4" style="background-color:#fdcb6e;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-20px;"></i>
+                      <div class="card-body bintang8">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                            <div class="col-lg-4 click" style="margin-top:8px;">
                                 <span class="mdi mdi-cart" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">PAY</h3>
+                              <h3 style="margin-top:15px; width:100%;" onclick="window.location.href='{{route('payment_order')}}'">Payment</h3>
                             </div>
                           </div>
                       </div>
@@ -274,13 +303,14 @@
               </div>
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-2" style="background-color:#00cec9;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-3px;"></i>
                       <div class="card-body bintang5">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-credit-card" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">RO</h3>
+                              <h3 style="margin-top:15px;" onclick="window.location.href='{{route('rencanapembelian')}}'">RO</h3>
                             </div>
                           </div>
                       </div>
@@ -288,13 +318,14 @@
               </div>
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-2" style="background-color:#e84393;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-3px;"></i>
                       <div class="card-body bintang6">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-credit-card" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">PO</h3>
+                              <h3 style="margin-top:15px;" onclick="window.location.href='{{route('purchaseorder')}}'">PO</h3>
                             </div>
                           </div>
                       </div>
@@ -302,13 +333,14 @@
               </div>
               <div class="col-lg-2 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-1" style="background-color:#ef6c57;">
+                  <i class="fa fa-star d-flex justify-content-end" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:1px;"></i>
                       <div class="card-body bintang7">
                           <div class="d-flex justify-content-center">
-                          <div class="col-lg-4 pl-2 click" style="margin-top:10px;">
+                          <div class="col-lg-4 pl-2 click" style="margin-top:8px;">
                                 <span class="mdi mdi-arrow-up-bold-box-outline menu-icon" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px;">Stok</h1>
+                              <h3 style="margin-top:15px;" onclick="window.location.href='{{route('stockgudang')}}'">Stok</h1>
                             </div>
                           </div>
                       </div>
@@ -316,13 +348,44 @@
               </div>
               <div class="col-lg-3 col-sm-4" style="margin-left:3em; margin-top:1em;">
                   <div class="card px-4" style="background-color:#f79f24;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-20px;"></i>
                       <div class="card-body bintang8">
                           <div class="d-flex justify-content-center">
-                            <div class="col-lg-4 click" style="margin-top:20px;">
+                            <div class="col-lg-4 click" style="margin-top:8px;">
                                 <span class="mdi mdi-chart-areaspline" style="color:white; font-size:30px;"></span>
                             </div>
                             <div class="col-lg-12" style="color:white;">
-                              <h3 style="margin-top:15px; width:100%;">Hutang-Piutang</h3>
+                              <h3 style="margin-top:15px; width:100%;">Hutang</h3>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-sm-4" style="margin-left:3em; margin-top:1em;">
+                  <div class="card px-4" style="background-color:#ffe98a">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-20px;"></i>
+                      <div class="card-body bintang8">
+                          <div class="d-flex justify-content-center">
+                            <div class="col-lg-4 click" style="margin-top:8px;">
+                                <span class="mdi mdi-chart-areaspline" style="color:white; font-size:30px;"></span>
+                            </div>
+                            <div class="col-lg-12" style="color:white;">
+                              <h3 style="margin-top:15px; width:100%;">Piutang</h3>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-sm-4" style="margin-left:3em; margin-top:1em;">
+                  <div class="card px-4" style="background-color:#d195f9;">
+                  <i class="fa fa-star d-flex justify-content-end light" aria-hidden="true" style="margin-bottom:-30px;  margin-top:5px; color:white; font-size:20px; margin-right:-20px;"></i>
+                      <div class="card-body bintang8">
+                          <div class="d-flex justify-content-center">
+                            <div class="col-lg-4 click" style="margin-top:8px;">
+                                <span class="mdi mdi-chart-areaspline" style="color:white; font-size:30px;"></span>
+                            </div>
+                            <div class="col-lg-12" style="color:white;">
+                              <h3 style="margin-top:15px; width:100%;">Omset</h3>
                             </div>
                           </div>
                       </div>
