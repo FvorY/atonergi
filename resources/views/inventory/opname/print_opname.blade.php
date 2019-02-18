8e64144a6b5a2cdb842593f161faf6498ea62ae4
foxbloodiron <ariakbar6944@gmail.com>
2019-02-18 15:33:39 +0700

    Update print_opname.blade.php

--- resources/views/inventory/opname/print_opname.blade.php
+++ resources/views/inventory/opname/print_opname.blade.php
@@ -269,7 +269,7 @@ table, td, th {
 						</tr>
 						<tr>
 							<td class="border-none"></td>
-							<td class="border-none"><img width="100" height="50" src="{{asset('assets/checked.jpg')}}"></td>
+							<td class="border-none"><img width="100" height="80" src="{{asset('assets/approved.png')}}"></td>
 							<td class="border-none"></td>
 						</tr>
 					</tbody>
