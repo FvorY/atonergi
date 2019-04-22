00a77907a92437d486c79b9a38ee32a32af0231a
FvorY <ferdyp73@gmail.com>
2019-04-22 10:56:11 +0700

    .

--- resources/views/quotation/q_quotation/edit_quotation.blade.php
+++ resources/views/quotation/q_quotation/edit_quotation.blade.php
@@ -189,55 +189,54 @@
               <div class="col-md-3 col-sm-6 col-xs-12">
                 <label>Type Quotation</label>
               </div>
-              @if ($data->q_status == 2)
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                  <div class="form-group type_q_div">
-                    <select style="width: 100%" class="form-control form-control-sm type_qo" id="type_qo" name="type_qo">
-                      <option value="0">--Select Type--</option>
-                      <option @if($data->q_type == 'SWP') selected="" @endif value="SWP">SWP</option>
-                      <option @if($data->q_type == 'WP') selected="" @endif value="WP">WP</option>
-                      <option @if($data->q_type == 'ACC') selected="" @endif value="ACC">Accesories</option>
-                      <option @if($data->q_type == 'OFD') selected="" @endif value="OFD">Off Grid</option>
-                      <option @if($data->q_type == 'ONG') selected="" @endif value="ONG">On Grid</option>
-                    </select>
-                    <label style="color: red" hidden  class="valid valid_2"><b>Type Quotation Harus Dipilih</b></label>
-                  </div>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="form-group type_q_div disabled">
+                  <select style="width: 100%" class="form-control form-control-sm type_qo" id="type_qo" name="type_qo">
+                    <option value="0">--Select Type--</option>
+                    <option @if($data->q_type == 'SWP') selected="" @endif value="SWP">SWP</option>
+                    <option @if($data->q_type == 'WP') selected="" @endif value="WP">WP</option>
+                    <option @if($data->q_type == 'ACC') selected="" @endif value="ACC">Accesories</option>
+                    <option @if($data->q_type == 'OFD') selected="" @endif value="OFD">Off Grid</option>
+                    <option @if($data->q_type == 'ONG') selected="" @endif value="ONG">On Grid</option>
+                  </select>
+                  <label style="color: red" hidden  class="valid valid_2"><b>Type Quotation Harus Dipilih</b></label>
                 </div>
-              @else
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                  <div class="form-group type_q_div disabled">
-                    <select style="width: 100%" class="form-control form-control-sm type_qo" id="type_qo" name="type_qo">
-                      <option value="0">--Select Type--</option>
-                      <option @if($data->q_type == 'SWP') selected="" @endif value="SWP">SWP</option>
-                      <option @if($data->q_type == 'WP') selected="" @endif value="WP">WP</option>
-                      <option @if($data->q_type == 'ACC') selected="" @endif value="ACC">Accesories</option>
-                      <option @if($data->q_type == 'OFD') selected="" @endif value="OFD">Off Grid</option>
-                      <option @if($data->q_type == 'ONG') selected="" @endif value="ONG">On Grid</option>
-                    </select>
-                    <label style="color: red" hidden  class="valid valid_2"><b>Type Quotation Harus Dipilih</b></label>
-                  </div>
-                </div>
-              @endif
-
+              </div>
               <div class="col-md-3 col-sm-0 col-xs-0">
                 <!-- empty -->
               </div>
               <div class="col-md-3 col-sm-6 col-xs-12">
                 <label>Type Product</label>
               </div>
-              <div class="col-md-6 col-sm-6 col-xs-12">
-                <div class="form-group type_p_div disabled">
-                  <select style="width: 100%" class="form-control form-control-sm type_p" id="type_p" name="type_p">
-                   <option value="0">--Select Type--</option>
-                   @foreach ($type_product as $val)
-                    <option @if ($val->it_code == $data->q_type_product)
-                     selected=""
-                    @endif value="{{$val->it_code}}">{{$val->it_code}}</option>
-                   @endforeach
-                  </select>
-                  <label style="color: red" hidden  class="valid valid_3"><b>Type Product Harus Dipilih</b></label>
+              @if ($data->q_status == 2)
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <div class="form-group type_p_div">
+                    <select style="width: 100%" class="form-control form-control-sm type_p" id="type_p" name="type_p">
+                     <option value="0">--Select Type--</option>
+                     @foreach ($type_product as $val)
+                      <option @if ($val->it_code == $data->q_type_product)
+                       selected=""
+                      @endif value="{{$val->it_code}}">{{$val->it_code}}</option>
+                     @endforeach
+                    </select>
+                    <label style="color: red" hidden  class="valid valid_3"><b>Type Product Harus Dipilih</b></label>
+                  </div>
                 </div>
-              </div>
+              @else
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <div class="form-group type_p_div disabled">
+                    <select style="width: 100%" class="form-control form-control-sm type_p" id="type_p" name="type_p">
+                     <option value="0">--Select Type--</option>
+                     @foreach ($type_product as $val)
+                      <option @if ($val->it_code == $data->q_type_product)
+                       selected=""
+                      @endif value="{{$val->it_code}}">{{$val->it_code}}</option>
+                     @endforeach
+                    </select>
+                    <label style="color: red" hidden  class="valid valid_3"><b>Type Product Harus Dipilih</b></label>
+                  </div>
+                </div>
+              @endif
             </div>
           </div>
 
