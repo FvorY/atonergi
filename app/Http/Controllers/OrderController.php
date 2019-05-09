8be9da686b49a994e8844c60470b4253ce33e53b
FvorY <ferdyp73@gmail.com>
2019-05-09 20:13:14 +0700

    .

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1282,7 +1282,7 @@ class OrderController extends Controller
         if ($so_dt != null or $wo_dt != null) {
           array_push($validation, 1);
         }
-	dd($valudation);
+	dd($validation);
         if (in_array(1, $validation)) {		
           if ($so->so_status == 'Printed' or $wo->wo_status == 'Printed') {
             return view('order/payment_order/detail_payment_order',compact('percent','item','data','data_dt','id','nota_po','market','nama_item','so','wo', 'akunKas', 'akunBank'));
