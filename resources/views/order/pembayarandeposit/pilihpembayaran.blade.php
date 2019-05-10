334d8ef68a0f62c76ad54b7878c331a7123e7dbb
FvorY <ferdyp73@gmail.com>
2019-05-10 10:02:59 +0700

    .

--- resources/views/order/pembayarandeposit/pilihpembayaran.blade.php
+++ resources/views/order/pembayarandeposit/pilihpembayaran.blade.php
@@ -40,7 +40,8 @@
           </div>
           <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="form-group">
-              @if ((int)$paydeposit == 0)
+              {{dd($paydeposit)}}
+              @if ($paydeposit == 0)
                 <input type="text" class="form-control-sm form-control" id="amount" name="amount" onblur="amountup()">
                 <input type="hidden" name="batasamount" id="batasamount">
               @else
